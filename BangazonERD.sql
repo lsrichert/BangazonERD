b36a51dde46da037c20abe19e622a73d5f762a97
Lauren Richert <lsrichert@gmail.com>
2018-09-09 13:05:54 -0500

    finished creating tables and inserting some data into each table

--- BangazonERD.sql
+++ BangazonERD.sql
@@ -74,6 +74,85 @@ CREATE TABLE `Computers` (
 `AssignmentStartDate` STRING,
 `AssignmentEndDate` STRING
 );
+INSERT INTO Computers (PurchaseDate)
+VALUES ("2015-09-01")
+;
+INSERT INTO Computers (PurchaseDate, AssignmentStartDate, AssignmentEndDate)
+VALUES ("2014-03-15", "2014-04-01", "2015-12-10")
+;
+INSERT INTO Computers (PurchaseDate,AssignmentStartDate)
+VALUES ("2018-05-01", "2018-05-15")
+;
+INSERT INTO Departments (Name, ExpenseBudget)
+VALUES ("Human Resources", 3500.00)
+;
+INSERT INTO Departments (Name, ExpenseBudget)
+VALUES ("Marketing", 5000.00)
+;
+INSERT INTO Departments (Name, ExpenseBudget)
+VALUES ("Purchasing", 2000.00)
+;
+INSERT INTO Customers (FirstName, LastName, RegistrationDate, LastLoginDate)
+VALUES ("Serena", "Williams,", "2012-03-13", "2018-08-25")
+;
+INSERT INTO Customers (FirstName, LastName, RegistrationDate, LastLoginDate)
+VALUES ("Naomi", "Osaka", "2015-06-30", "2018-02-20")
+;
+INSERT INTO Customers (FirstName, LastName, RegistrationDate, LastLoginDate)
+VALUES ("Sloane", "Stephens", "2018-01-15", "2018-02-14")
+;
+INSERT INTO Employees (DepartmentId, FirstName, LastName, Supervisor)
+VALUES (1, "Denise", "Henry", 1)
+;
+INSERT INTO Employees (DepartmentId, FirstName, LastName, Supervisor)
+VALUES (3, "Lucy", "Davis", 0)
+;
+INSERT INTO Employees (DepartmentId, FirstName, LastName, Supervisor)
+VALUES (2, "Elizabeth", "Barnett", 0)
+;
+INSERT INTO Orders (CustomerId, PaymentTypeId)
+VALUES (1, 1)
+;
+INSERT INTO Orders (CustomerId, PaymentTypeId)
+VALUES (4, 2)
+;
+INSERT INTO Orders (CustomerId, PaymentTypeId)
+VALUES (5, 1)
+;
+INSERT INTO PaymentTypes (CustomerId, PaymentName, AccountNumber)
+VALUES (1, "Visa", 138577)
+;
+INSERT INTO PaymentTypes (CustomerId, PaymentName, AccountNumber)
+VALUES (4, "MasterCard", 894785)
+;
+INSERT INTO ProductTypes (Name)
+VALUES ("Toys")
+;
+INSERT INTO ProductTypes (Name)
+VALUES ("Electronics")
+;
+INSERT INTO ProductTypes (Name)
+VALUES ("Outdoor")
+;
+INSERT INTO Products (ProductTypeId, CustomerId, Title, Description, Price, Qty)
+VALUES (1, 1, "Teddy Ruxpin", "Stuffed Bear", 14.00, 5)
+;
+INSERT INTO Products (ProductTypeId, CustomerId, Title, Description, Price, Qty)
+VALUES (2, 4, "Samsung 50 TV", "Color TV HD", 1800.00, 8)
+;
+INSERT INTO Products (ProductTypeId, CustomerId, Title, Description, Price, Qty)
+VALUES (3, 1, "Kelty 4-Person", "Tent", 259.00, 3)
+;
+INSERT INTO TrainingPrograms (Name, StartDate, EndDate, MaximumAttendees)
+VALUES ("Product Knowledge", "2018-11-14", "2018-11-30", 20)
+;
+INSERT INTO TrainingPrograms (Name, StartDate, EndDate, MaximumAttendees)
+VALUES ("Workplace Harrassment", "2018-10-15", "2018-10-30", 25)
+;
+INSERT INTO TrainingPrograms (Name, StartDate, EndDate, MaximumAttendees)
+VALUES ("Customer Service", "2018-09-01", "2018-09-15", 22)
+;
 
 
-`
+
+
